a60c7ebc4c8ef40b7b5479a00daab0b39a8efcc8
Dand <152787216+Dand47@users.noreply.github.com>
2026-06-30 02:52:21 +0300

    Update start_end.py
    Убран лишний метод

--- start_end.py
+++ start_end.py
@@ -16,12 +16,8 @@ _____|_____
         """)
 
 
-    def game_start_or_end(self):
-        return input("Желаете начать новую игру или выйти из приложения? y/n ").lower().strip()
-
-    def end(self):
-        return input("Конец игры хотите продолжить игру? y/n ").lower().strip()
-
+    def game_start_end(self):
+        return input("Конец игры, желаете начать новую игру или выйти из приложения? y/n ").lower().strip()
 
 
 
