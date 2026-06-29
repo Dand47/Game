304c5f07b55d7a006f5d92dcdcca0c023ba77299
Dand <152787216+Dand47@users.noreply.github.com>
2026-06-30 02:49:32 +0300

    Update main.py
    Добавил несколько проверок и убрал наследование и сделал композицию

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
-from random_slovo import RandomWord
+from random_word import RandomWord
 from start_end import Start
 from visual import visual
 
 
 
 
-class Game(Start, RandomWord):
-
+class Game:
 
     def __init__(self):
-        self.filename = 'text.txt'
+        self.start = Start()
+        self.random_word = RandomWord(filename='text.txt')
+
+
         self.wins = 0
         self.lose = 0
         self.hp = 5
-        self.main_words = 'Минато'
-        self.user_words = []
+        self.secret_words = 'минато'
+        self.user_input = []
 
 
     def setup_game(self):
-        self.hp = 5
-
         try:
-            self.main_words = self.read_text()
-            self.user_words = ['*'] * len(self.main_words)
-        except:
-            print('возникла ошибка в setup_game')
+            self.hp = 5
+            self.secret_words = self.random_word.read_text()
+            self.user_input = ['*'] * len(self.secret_words)
+            return True
+        except (TypeError, ValueError, IndexError, FileNotFoundError):
+            print('Ошибка в setup_game')
+            return False
 
 
     @staticmethod
@@ -33,11 +36,11 @@ class Game(Start, RandomWord):
 
 
     def main_game(self, words):
-        for i in range(len(self.main_words)):
-             if self.main_words[i].lower() == words:
-                 self.user_words[i] = self.main_words[i]
+        for i in range(len(self.secret_words)):
+             if self.secret_words[i].lower() == words:
+                 self.user_input[i] = self.secret_words[i]
 
-        print(f"Правильно: {self.user_words}")
+        print(f"Правильно: {self.user_input}")
 
 
     def lose_hp(self):
@@ -52,16 +55,16 @@ class Game(Start, RandomWord):
             print('Нужно вводить букву!')
             return
 
-        elif user_input in self.user_words:
-            print(f"Вы уже вводили эту букву!")
-            return
-
-
         elif len(user_input) > 1:
             print('Нужно ввести 1 букву!')
             return
 
-        elif user_input in self.main_words.lower():
+        elif user_input in self.user_input:
+            print(f"Вы уже вводили эту букву!")
+            return
+
+
+        elif user_input in self.secret_words.lower():
             self.main_game(user_input)
             return
 
@@ -70,7 +73,7 @@ class Game(Start, RandomWord):
 
 
     def check_win(self):
-        return "".join(self.user_words) == self.main_words
+        return "".join(self.user_input).lower() == self.secret_words.lower()
 
 
     def check_lose(self):
@@ -78,64 +81,39 @@ class Game(Start, RandomWord):
 
 
     def game_start(self):
-        self.setup_game()
+        if self.setup_game():
+            while True:
+                user_input = self.get_input()
+                self.check_input(user_input)
 
-        while True:
-            user_input = self.get_input()
-            self.check_input(user_input)
+                if self.check_win():
+                    self.wins += 1
+                    print(f"Победа!: побед {self.wins}")
+                    break
 
-            if self.check_win():
-                self.wins += 1
-                print(f"Победа!: побед {self.wins}")
-                break
+                if self.check_lose():
+                    self.lose += 1
+                    print(f"Поражение: поражений {self.lose}")
+                    break
 
-            if self.check_lose():
-                self.lose += 1
-                print(f"Поражение: поражений {self.lose}")
-                break
+
+
+
+
+
+
+
+
+
 
 
 
 if __name__ == '__main__':
+
     game = Game()
-    game.hello()
+    game.start.hello()
 
     while True:
-
-        if game.game_start_or_end() != 'y':
-            break
+        if game.start.game_start_end() != 'y':
+             break
         game.game_start()
-        if game.end() != 'y':
-            break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
